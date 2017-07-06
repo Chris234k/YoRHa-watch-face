29e3f4382cf14acadff271dacd87d77df4979a6f
Chris Bruno <Chris234k@gmail.com>
2017-07-05 21:40:20 -0400

    Structure changes, needs testing and tweaks

--- wear/src/main/java/chris234k/yorhawatchface/GlitchTextWriter.java
+++ wear/src/main/java/chris234k/yorhawatchface/GlitchTextWriter.java
@@ -15,81 +15,72 @@ interface ICompletionCallback {
 
 
 public class GlitchTextWriter {
-    private boolean mIsAnimating;
+    private boolean mIsAnimating, canUpdate;
     private static final int FRAMES_PER_INDEX = 2;
     private int mTextIndex, mFrameIndex;
     private String mTextContent;
     private StringBuilder mTextValue;
-    private Handler mHandler;
-    private final Runnable mRunnable;
-    private final long mTextDrawRate;
 
     private ICompletionCallback mCompletionCallback;
 
     private static final String RANDOM_NUMERIC = "1234567890:";
 
-    public GlitchTextWriter(long textDrawRate) {
+    public GlitchTextWriter() {
         mTextIndex = 0;
         mFrameIndex = 0;
         mTextValue = new StringBuilder();
-        mTextDrawRate = textDrawRate;
-
-        mHandler = new Handler();
-        mRunnable = new Runnable() {
-            @Override
-            public void run() {
-                // Consecutively display each character in the string
-                //
-                // For index x
-                // Frame 1- display random letter at x
-                // Frame 2- display value at index 0 at x
-                // Advance index
-                //
-                // This gives the appearance that index 0 is moving through the string to populate it
-
-                mIsAnimating = true;
-
-                char insertChar;
-
-                if(mFrameIndex < FRAMES_PER_INDEX) {
-                    // Roll random character to display for current index
-                    Random r = new Random();
-                    int randomNum = r.nextInt(RANDOM_NUMERIC.length());
-                    insertChar = RANDOM_NUMERIC.charAt(randomNum);
-                } else {
-                    insertChar = mTextContent.charAt(0);
-                }
-
-                // If the string is long enough to pull a sub string from
-                if(mTextIndex > 0) {
-                    // Pull substring
-                    String subStr = mTextContent.substring(0, mTextIndex);
-                    mTextValue.replace(0, mTextValue.length(), subStr);
-                    // Replace last char in sub w/ random
-                    mTextValue.setCharAt(mTextIndex -1, insertChar);
-                } else{
-                    // Just use blank string
-                    mTextValue.replace(0, mTextValue.length(), "");
-                }
-
-                Log.d("yorhawatchface", mTextIndex + " " + mTextValue);
-
-                if(mFrameIndex == FRAMES_PER_INDEX){
-                    mTextIndex++;
-                    mFrameIndex = 1;
-                } else {
-                    mFrameIndex++;
-                }
-
-                if(mTextIndex <= mTextContent.length()) {
-                    mHandler.postDelayed(mRunnable, mTextDrawRate);
-                } else {
-                    mIsAnimating = false;
-                    mCompletionCallback.onComplete();
-                }
-            }
-        };
     }
+
+    public void update() {
+        if(mIsAnimating) {
+            // Consecutively display each character in the string
+            //
+            // For index x
+            // Frame 1- display random letter at x
+            // Frame 2- display value at index 0 at x
+            // Advance index
+            //
+            // This gives the appearance that index 0 is moving through the string to populate it
+
+            char insertChar;
+
+            if (mFrameIndex < FRAMES_PER_INDEX) {
+                // Roll random character to display for current index
+                Random r = new Random();
+                int randomNum = r.nextInt(RANDOM_NUMERIC.length());
+                insertChar = RANDOM_NUMERIC.charAt(randomNum);
+            } else {
+                insertChar = mTextContent.charAt(0);
+            }
+
+            // If the string is long enough to pull a sub string from
+            if (mTextIndex > 0) {
+                // Pull substring
+                String subStr = mTextContent.substring(0, mTextIndex);
+                mTextValue.replace(0, mTextValue.length(), subStr);
+                // Replace last char in sub w/ random
+                mTextValue.setCharAt(mTextIndex - 1, insertChar);
+            } else {
+                // Just use blank string
+                mTextValue.replace(0, mTextValue.length(), "");
+            }
+
+            Log.d("yorhawatchface", mTextIndex + " " + mTextValue);
+
+            if (mFrameIndex == FRAMES_PER_INDEX) {
+                mTextIndex++;
+                mFrameIndex = 1;
+            } else {
+                mFrameIndex++;
+            }
+
+            if (mTextIndex > mTextContent.length()) {
+                mIsAnimating = false;
+                mCompletionCallback.onComplete();
+            }
+        }
+    }
+
 
     public boolean getIsAnimating() {
         return mIsAnimating;
@@ -99,7 +90,7 @@ public class GlitchTextWriter {
         return mTextValue.toString();
     }
 
-    public void animateText(String text, long delayMillis, ICompletionCallback completionCallback) {
+    public void animateText(String text, ICompletionCallback completionCallback) {
         // Don't allow animations to be interrupted, stopAnimation should be called directly.
         if(!mIsAnimating) {
             mTextContent = text;
@@ -108,16 +99,10 @@ public class GlitchTextWriter {
             mFrameIndex = 0;
             mIsAnimating = true;
             mCompletionCallback = completionCallback;
-
-            mHandler.removeCallbacks(mRunnable);
-            mHandler.postDelayed(mRunnable, delayMillis);
         }
     }
 
     public void stopAnimation() {
-        if(mIsAnimating) {
-            mHandler.removeCallbacks(mRunnable);
-            mIsAnimating = false;
-        }
+        mIsAnimating = false;
     }
 }
