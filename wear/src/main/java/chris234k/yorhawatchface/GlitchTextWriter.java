663c260c319e0ff89b940bc5c9b9e45ad3505f62
Chris Bruno <Chris234k@gmail.com>
2018-01-15 11:27:48 -0500

    Cleaning up GlitchTextWriter

--- wear/src/main/java/chris234k/yorhawatchface/GlitchTextWriter.java
+++ wear/src/main/java/chris234k/yorhawatchface/GlitchTextWriter.java
@@ -18,77 +18,94 @@ public class GlitchTextWriter {
     private boolean mIsAnimating;
     private static final int FRAMES_PER_INDEX = 2;
     private int mTextIndex, mFrameIndex;
-    private String mTextContent;
-    private StringBuilder mTextValue;
+    private String mFullText; // The end result, the string we're building towards
+    private StringBuilder mCurrentText; // Current string value, as it animates toward mFulString
     private Handler mHandler;
     private final Runnable mRunnable;
     private final long mTextDrawRate;
 
     private ICompletionCallback mCompletionCallback;
 
+    // NieR selects random characters from the end result string (mFullText)
+    // Instead, we select random characters from all valid time string values
+    // With numbers, the effect is much less pronounced:
+    // 10:00:00 only has 3 unique chars
     private static final String RANDOM_NUMERIC = "1234567890:";
 
     public GlitchTextWriter(long textDrawRate) {
-        mTextIndex = 0;
+        mTextIndex = 1;
         mFrameIndex = 1;
-        mTextValue = new StringBuilder();
+        mCurrentText = new StringBuilder();
         mTextDrawRate = textDrawRate;
 
         mHandler = new Handler();
         mRunnable = new Runnable() {
             @Override
             public void run() {
-            // Consecutively display each character in the string
-            //
-            // For index x
-            // Draw correct letters from 0 to x-1
-            // Frame 1- display random letter at x
-            // Frame 2- display value at index 0 at x
-            // Advance index
-            //
-            // This gives the appearance that index 0 is moving through the string to populate it
+
+                // Here's an example progression from NieR
+                // L
+                // a
+                // Lg
+                // LL
+                // LaL
+                // Laa
+                // Lang
+                // LanL
+                // Langg
+                // Langg
+                // LanguL
+                // Langue
+                // LanguaL
+                // Languag
+                // Languaga
+                // Language
+                //
+                // Consecutively display each character in the string
+                //
+                // For index x
+                // Draw correct letters from 0 to x-1
+                // Frame 1- display random letter at x
+                // Frame 2- display value at index 0 at x
+                // Advance index
+                //
+                // This gives the appearance that index 0 is moving through the string to populate it
 
                 mIsAnimating = true;
 
-            char insertChar;
+                char insertChar;
 
-            if (mFrameIndex < FRAMES_PER_INDEX) {
-                // Roll random character to display for current index
-                Random r = new Random();
-                int randomNum = r.nextInt(RANDOM_NUMERIC.length());
-                insertChar = RANDOM_NUMERIC.charAt(randomNum);
-            } else {
-                insertChar = mTextContent.charAt(0);
-            }
+                if (mFrameIndex < FRAMES_PER_INDEX) {
+                    // Roll random character to display for current index
+                    Random r = new Random();
+                    int randomNum = r.nextInt(RANDOM_NUMERIC.length());
+                    insertChar = RANDOM_NUMERIC.charAt(randomNum);
+                } else {
+                    insertChar = mFullText.charAt(0);
+                }
 
-            // If the string is long enough to pull a sub string from
-            if (mTextIndex > 0) {
-                // Pull substring
-                String subStr = mTextContent.substring(0, mTextIndex);
-                mTextValue.replace(0, mTextValue.length(), subStr);
-                // Replace last char in sub w/ random
-                mTextValue.setCharAt(mTextIndex - 1, insertChar);
-            } else {
-                // Just use blank string
-                mTextValue.replace(0, mTextValue.length(), "");
-            }
+                // Pull substring (assumes mTextIndex > 0)
+                String subStr = mFullText.substring(0, mTextIndex);
+                mCurrentText.replace(0, mCurrentText.length(), subStr);
+                // Replace last char in sub
+                mCurrentText.setCharAt(mTextIndex - 1, insertChar);
 
-//            Log.d("yorhawatchface", mTextIndex + " " + mTextValue);
+//                Log.d("yorhawatchface", mTextIndex + " " + mCurrentText);
 
-            if (mFrameIndex == FRAMES_PER_INDEX) {
-                mTextIndex++;
-                mFrameIndex = 1;
-            } else {
-                mFrameIndex++;
-            }
+                if (mFrameIndex == FRAMES_PER_INDEX) {
+                    mTextIndex++;
+                    mFrameIndex = 1;
+                } else {
+                    mFrameIndex++;
+                }
 
-                if(mTextIndex <= mTextContent.length()) {
+                if(mTextIndex <= mFullText.length()) {
                     mHandler.postDelayed(mRunnable, mTextDrawRate);
                 } else {
-                mIsAnimating = false;
-                mCompletionCallback.onComplete();
+                    mIsAnimating = false;
+                    mCompletionCallback.onComplete();
+                }
             }
-        }
         };
     }
 
@@ -97,15 +114,15 @@ public class GlitchTextWriter {
     }
 
     public String getTextValue() {
-        return mTextValue.toString();
+        return mCurrentText.toString();
     }
 
     public void animateText(String text, long delayMillis, ICompletionCallback completionCallback) {
         // Don't allow animations to be interrupted, stopAnimation should be called directly.
         if(!mIsAnimating) {
-            mTextContent = text;
-            mTextValue.replace(0, mTextValue.length(), "");
-            mTextIndex = 0;
+            mFullText = text;
+            mCurrentText.replace(0, mCurrentText.length(), "");
+            mTextIndex = 1;
             mFrameIndex = 1;
             mIsAnimating = true;
             mCompletionCallback = completionCallback;
@@ -118,7 +135,7 @@ public class GlitchTextWriter {
     public void stopAnimation() {
         if(mIsAnimating) {
             mHandler.removeCallbacks(mRunnable);
-        mIsAnimating = false;
+            mIsAnimating = false;
+        }
     }
-}
 }
